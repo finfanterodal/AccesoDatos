1b415537539d11daa5613098dab13d93f9a05dcf
oracle <oracle@localhost>
2019-11-14 14:45:31 +0100

    Init!

---
+++ Exam_Example2/src/exam_example2/Exam_Example2.java
@@ -0,0 +1,16 @@
+package exam_example2;
+
+/**
+ *
+ * @author oracle
+ */
+public class Exam_Example2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}
